c56a94cc0743be1294a860ecc4cdc51df06be539
Parimarjan Negi <pnegi@stanford.edu>
2019-07-01 13:41:59 -0400

    updated install scripts

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,7 @@ sudo apt --assume-yes install default-jre maven docker.io python3-pip fish
 git clone https://github.com/park-project/park.git ~/park
 cd ~/park
 git checkout qopt
+# because repo is private, just syncing it directly
 mv ~/payload/park_agents/ ~/park_agents
 cd ~/park_agents
 git checkout qopt-agent
@@ -10,3 +11,9 @@ pip3 install -r requirements.txt
 cd ~/
 git clone https://github.com/hongzimao/gcn_pytorch.git ~/gcn_pytorch
 echo "export PYTHONPATH=$PYTHONPATH:/home/ubuntu/park/:/home/ubuntu/gcn_pytorch" >> ~/.bashrc
+cd ~/
+git clone https://github.com/parimarjan/learned-cardinalities.git 
+cd learned-cardinalities
+pip3 install -r requirements.txt
+
+# TODO: set up postgres via docker etc.
