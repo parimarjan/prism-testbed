9c9132d2fde2a856d4a3b65c9058f1bdbc69cbca
parimarjan <parimarjannegi@gmail.com>
2020-10-21 01:50:48 -0400

    minor

--- scripts/start_rt.sh
+++ scripts/start_rt.sh
@@ -3,5 +3,5 @@ cd ~/learned-cardinalities
 
 python3 get_runtimes.py --results_dir /home/ubuntu/payload/results/ \
   --results_fn cm1_jerr.pkl --cost_model cm1 --materialize 0 \
-  --db_name so --timeout 3600000 --rerun_timeouts 1
+  --db_name so --timeout 900000 --rerun_timeouts 0
 
